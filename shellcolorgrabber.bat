376c8bc79c4ed91078fa05ee3777abeabef08d9e
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2020-05-01 16:40:05 -0500

    working color grabber

--- shellcolorgrabber.bat
+++ shellcolorgrabber.bat
@@ -1,9 +1,7 @@
 @echo off
 
 set port=%1
-set x=%2
-set y=%3
-set offset=%4
+set offset=%2
 
 cd tools
-adb -s localhost:%port% shell "screencap /sdcard/screencap.dump; dd if="/sdcard/screencap.dump" bs=4 count=1 skip=%offset% 2>/dev/null | hexdump -C"
+adb -s localhost:%port% shell "screencap /sdcard/screencap.dump; dd if=""/sdcard/screencap.dump"" bs=4 count=1 skip=%offset% 2>/dev/null | hexdump -C"
--- shellinterpreter.py
+++ shellinterpreter.py
@@ -2,17 +2,25 @@ import subprocess
 import random
 from sys import argv
 
-interact_x, interact_y = argv
+script, interact_x, interact_y = argv
 
-emulator_port = 5555 #hard coded for testing allows for all emulators to be interacted with apon change
-resolution_width = 960
-resolution_height = 540
+emulator_port = int(5555) #hard coded for testing allows for all emulators to be interacted with apon change
+interact_x = int(interact_x)
+interact_y = int(interact_y)
+resolution_width = int(960)
+resolution_height = int(540)
 stdoutlineformatted = "" #formatted byte string from byte variable stdoutline
 
-item = subprocess.Popen(["shellcolorgrabber.bat", str(emulator_port), str(interact_x),str(interact_y),str(resolution_width * interact_x + interact_y)] , 
-                         shell=True, stdout=subprocess.PIPE) #launch subprocess to send commands to adb using .bat files
+if interact_x > resolution_width or interact_y > resolution_height:
+    print("Interaction location to great!")
+
+item = subprocess.Popen(["shellcolorgrabber.bat", str(emulator_port), str(resolution_width * interact_y + interact_x)], shell=True, stdout=subprocess.PIPE) #launch subprocess to send commands to adb using .bat files
 for stdoutline in item.stdout:
     stdoutlineformatted += str(stdoutline, 'utf-8')
 
-temp = "".join(stdoutlineformatted[10:18].split()) #grab the bytes only needed which are RGB seperated by spaces and strip the whitespace
+temp = "".join(stdoutlineformatted[stdoutlineformatted.find('00000000') + 10:stdoutlineformatted.find('ff')].split()) #grab the bytes only needed which are RGB seperated by spaces and strip the whitespace
+
+if temp == '5a3a2e':
+    item = subprocess.Popen(["shellinputtap.bat", str(emulator_port), str(interact_x), str(interact_y)], shell=True, stdout=subprocess.PIPE)
+
 print(temp)
